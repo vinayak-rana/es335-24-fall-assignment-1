5f16277527f7e050c746d8a3ebda4a96b113f604
vinny <vinayakrana4444@gmail.com>
2024-08-26 13:17:35 +0530

    Innitial commit for Decision Tree

--- tree/base.py
+++ tree/base.py
@@ -280,18 +280,3 @@ class DecisionTree():
                 self.plot(node.right_child,depth+1)
 
         
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.datasets import load_iris
-# iris=load_iris()
-# x=iris.data
-# y=iris.target
-
-# from sklearn.model_selection import train_test_split
-# x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3)
-# # tree=DecisionTreeClassifier(criterion='gini')
-# tree=DecisionTree(criterion='information_gain',max_depth=3)
-# tree.fit(x_train,y_train)
-# predictions=tree.predict(x_test)
-# from sklearn.metrics import classification_report
-# print(classification_report(y_test,predictions))
-# tree.plot()
